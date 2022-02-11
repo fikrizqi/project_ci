2dc28894187e0dd41ae631f7b98b367ba1c4d437
fikri <fikrimassamu071103@gmail.com>
2022-02-11 11:24:42 +0700

    first commit

---
+++ application/views/auth/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <center>
+        <h3>Form login</h3>
+        <?php if(!empty($output)):?>
+            <?php echo $output ?>
+        <?php endif?>
+    <form action="" method="post">
+        <label for="">Username</label><br>
+        <input type="text" name="name" required><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" required><br>
+        <button type="submit">Login</button>
+    </form>
+    </center>
+</body>
+</html>
